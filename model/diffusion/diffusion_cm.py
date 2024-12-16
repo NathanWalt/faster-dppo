548803d178913bf97b04646361591906acab05f4
Wang Weikang <wwk22@mails.tsinghua.edu.cn>
2024-12-17 00:48:38 +0800

    fix sample method in CM training

--- model/diffusion/diffusion_cm.py
+++ model/diffusion/diffusion_cm.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from model.diffusion.diffusion import Sample
+from model.diffusion.diffusion_ct import CTDiffusionModel
 
 log = logging.getLogger(__name__)
 
@@ -29,7 +30,7 @@ class CTConsistencyModel(nn.Module):
         self.consistency_lambda = consistency_lambda
         self.sampling_steps = sampling_steps
         self.horizon_steps = horizon_steps
-        
+
         # Set up models
         self.network = network.to(device)
         # if network_path is not None:
@@ -42,7 +43,7 @@ class CTConsistencyModel(nn.Module):
         logging.info(
             f"Number of network parameters: {sum(p.numel() for p in self.parameters())}"
         )
-        
+
     def get_c(self,t):
         sigma_data = torch.tensor(0.5)
         a = (sigma_data * sigma_data)/(t * t + sigma_data * sigma_data)
@@ -52,34 +53,35 @@ class CTConsistencyModel(nn.Module):
     def forward(self, cond, deterministic=True, t = None, x = None):
         device = self.device
         B = len(cond["state"])
-        
+
         if t is None:
             t = torch.ones((B, 1, 1), device=device)
-        
+
         if x is None:
             x = torch.randn((B, self.horizon_steps, self.action_dim), device=device)
-        
+
         a, b = self.get_c(t)
         x = a * x + b * self.network(x, t, cond)
-        
+
         return Sample(x, None)
 
     def loss(self, teacher_model, ema_model, sampling_steps, x, *args):
         return self.consistency_loss(ema_model, teacher_model, sampling_steps, x, *args)
-    
+
     def get_t_i(self, i, sampling_steps):
         return i / sampling_steps
-    
-    def consistency_loss(self, ema_model, teacher_model, sampling_steps, x, cond: dict):
+
+    def consistency_loss(self, ema_model, teacher_model : CTDiffusionModel, sampling_steps, x, cond: dict):
         batch_size = x.shape[0]
         n = torch.randint(1, sampling_steps, (batch_size, 1, 1)).cuda()
-        
-        x_tn1 = x + torch.randn_like(x) * torch.sqrt(self.get_t_i(n+1, sampling_steps)**2)
+
+        tn1 = self.get_t_i(n+1, sampling_steps)
+        x_tn1 = teacher_model.q_sample(x, tn1)
         x_tn_phi = x_tn1 + (self.get_t_i(n, sampling_steps) - self.get_t_i(n+1, sampling_steps)) * teacher_model.pfode_dxt(x_tn1, torch.tensor(self.get_t_i(n+1, sampling_steps)), cond)
-        
+
         a, b = self.get_c(self.get_t_i(n+1, sampling_steps))
         f_theta = a * x + b * self.network(x_tn1, self.get_t_i(n+1, sampling_steps), cond=cond)
-        
+
         a, b = self.get_c(self.get_t_i(n, sampling_steps))
         f_theta_minus = a * x_tn_phi + b * ema_model.network(x_tn_phi, self.get_t_i(n, sampling_steps), cond=cond)
 

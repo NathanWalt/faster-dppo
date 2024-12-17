fix loss calculation in CM training

model/diffusion/diffusion_cm.py
@@ -30,7 +30,7 @@ class CTConsistencyModel(nn.Module):
         self.consistency_lambda = consistency_lambda
         self.sampling_steps = sampling_steps
         self.horizon_steps = horizon_steps
-        
+
         # Set up models
         self.network = network.to(device)
         # if network_path is not None:
@@ -43,7 +43,7 @@ class CTConsistencyModel(nn.Module):
         logging.info(
             f"Number of network parameters: {sum(p.numel() for p in self.parameters())}"
         )
-        
+
     def get_c(self,t):
         sigma_data = torch.tensor(0.5)
         a = (sigma_data * sigma_data)/(t * t + sigma_data * sigma_data)
@@ -53,41 +53,43 @@ class CTConsistencyModel(nn.Module):
     def forward(self, cond, deterministic=True, t = None, x = None):
         device = self.device
         B = len(cond["state"])
-        
+
         if t is None:
             t = torch.ones((B, 1, 1), device=device)
-        
+
         if x is None:
             x = torch.randn((B, self.horizon_steps, self.action_dim), device=device)
-        
+
         a, b = self.get_c(t)
         x = a * x + b * self.network(x, t, cond)
-        
+
         return Sample(x, None)
 
     def loss(self, teacher_model, ema_model, sampling_steps, x, *args):
         return self.consistency_loss(ema_model, teacher_model, sampling_steps, x, *args)
-    
+
     def get_t_i(self, i, sampling_steps):
         return i / sampling_steps
-    
+
     def consistency_loss(self, ema_model, teacher_model:CTDiffusionModel, sampling_steps, x, cond: dict):
         batch_size = x.shape[0]
         n = torch.randint(1, sampling_steps, (batch_size, 1, 1)).cuda()
-        
+
         tn1 = self.get_t_i(n+1, sampling_steps)
         x_tn1 = teacher_model.q_sample(x, tn1, None)
         x_tn_phi = x_tn1 + (self.get_t_i(n, sampling_steps) - self.get_t_i(n+1, sampling_steps)) * teacher_model.pfode_dxt(x_tn1, torch.tensor(self.get_t_i(n+1, sampling_steps)), cond)
-        
+
         # a, b = self.get_c(self.get_t_i(n+1, sampling_steps))
-    
+
+        max_std = teacher_model.get_std(torch.tensor(1.0))
         a = teacher_model.get_mu_coeff(self.get_t_i(n+1, sampling_steps))
-        b = teacher_model.get_std(self.get_t_i(n+1, sampling_steps))/teacher_model.get_std(torch.tensor(1.0))
-        f_theta = a * x + b * self.network(x_tn1, self.get_t_i(n+1, sampling_steps), cond=cond)
-        
+        b = teacher_model.get_std(self.get_t_i(n+1, sampling_steps)) / max_std
+        f_theta = a * x_tn1 + b * self.network(x_tn1, self.get_t_i(n+1, sampling_steps), cond=cond)
+
         a = teacher_model.get_mu_coeff(self.get_t_i(n, sampling_steps))
-        b = teacher_model.get_std(self.get_t_i(n, sampling_steps))/teacher_model.get_std(torch.tensor(1.0))
-        f_theta_minus = a * x_tn_phi + b * ema_model.network(x_tn_phi, self.get_t_i(n, sampling_steps), cond=cond)
+        b = teacher_model.get_std(self.get_t_i(n, sampling_steps)) /max_std
+        with torch.no_grad():
+            f_theta_minus = a * x_tn_phi + b * ema_model.network(x_tn_phi, self.get_t_i(n, sampling_steps), cond=cond)
 
         loss = F.mse_loss(f_theta, f_theta_minus, reduction="mean")
         return self.consistency_lambda * loss
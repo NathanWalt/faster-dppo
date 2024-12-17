6d1084c1cf7d1196319c16f070d2d0c9d39d900e
Wang Weikang <wwk22@mails.tsinghua.edu.cn>
2024-12-17 11:23:44 +0800

    fix configuration in CM training

--- agent/pretrain/distilation_agent.py
+++ agent/pretrain/distilation_agent.py
@@ -23,62 +23,19 @@ class ConsistencyDistillationAgent(PreTrainAgent):
         self.teacher_model.load_state_dict(data["model"])
         self.teacher_model.requires_grad_(False)
         self.ema_model.requires_grad_(False)
-        
+
         self.learning_rate = cfg.train.learning_rate
         self.d = nn.MSELoss()
         self.lambda_ = 1.0
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        
-    def get_t_i(self, i):
-        return 1.0 / i
 
-    # def train(self, epochs):
-    #     for epoch in range(epochs):
-    #         for batch_train in self.dataloader_train:
-    #             # Sample from the dataset and uniform distribution for time step
-    #             n = torch.randint(1, self.teacher_model.sampling_steps - 1, (1,)).item()
-                
-    #             # Sample noise from normal distribution
-    #             x_tn1 = x + torch.randn_like(x) * torch.sqrt(self.get_t_i(n+1)**2)
-                
-    #             # Use ODE solver to get the reconstructed state at time t_n
-    #             x_tn_phi = x_tn1 + (self.get_t_i(n) - self.get_t_i(n+1)) * self.teacher_model.pfode_dxt(x_tn1, self.get_t_i(n+1))
-                
-    #             # Calculate the loss between the model's output and the teacher model's output
-    #             loss = self.calculate_loss(x_tn1, x_tn_phi, self.get_t_i(n), self.get_t_i(n+1))
-                
-    #             # Backpropagate the loss and update the model's parameters
-    #             self.optimizer.zero_grad()
-    #             loss.backward()
-    #             self.optimizer.step()
-                
-    #             # Update the teacher model's EMA parameters
-    #             self.update_ema_params()
-            
-    #         # Log the loss at the end of each epoch
-    #         print(f"Epoch {epoch}, Loss: {loss.item()}")
-
-    # def calculate_loss(self, x_tn1, x_tn_phi, t_n, t_n1):
-    #     # Calculate the outputs of the student and teacher models
-    #     f_theta = self.model(x_tn1, t = t_n1)
-    #     f_theta_minus = self.model_ema(x_tn_phi, t = t_n)
-        
-    #     # Calculate the consistency loss
-    #     loss = self.lambda_(t_n) * self.d(f_theta, f_theta_minus)
-    #     return loss
-
-    # def update_ema_params(self):
-    #     # Update the teacher model's parameters using EMA
-    #     for name, param in self.ema_model.named_parameters():
-    #         param.data = self.mu * param.data + (1 - self.mu) * self.model.state_dict()[name]
-        
     # --------------------- RUN -------------------------#
     def run(self):
 
         timer = Timer()
         self.epoch = 1
         for _ in range(self.n_epochs):
-            
+
             # train
             loss_train_epoch = []
             for batch_train in self.dataloader_train:
@@ -86,8 +43,8 @@ class ConsistencyDistillationAgent(PreTrainAgent):
                     batch_train = batch_to_device(batch_train)
 
                 self.model.train()
-                
-                loss_train = self.model.loss(self.teacher_model, self.ema_model, self.teacher_model.sampling_steps, *batch_train)
+
+                loss_train = self.model.loss(self.teacher_model, self.ema_model, *batch_train)
                 loss_train.backward()
                 loss_train_epoch.append(loss_train.item())
 
@@ -95,7 +52,7 @@ class ConsistencyDistillationAgent(PreTrainAgent):
                 self.optimizer.zero_grad()
 
                 # update ema
-                
+
                 self.step_ema()
             loss_train = np.mean(loss_train_epoch)
 
--- model/diffusion/diffusion_cm.py
+++ model/diffusion/diffusion_cm.py
@@ -18,7 +18,7 @@ class CTConsistencyModel(nn.Module):
         horizon_steps,
         network_path=None,
         device="cuda:0",
-        sampling_steps = 100,
+        sampling_steps = 1000,
         # Consistency parameters
         consistency_lambda=1.0,  # Consistency loss weight
         **kwargs,
@@ -29,16 +29,21 @@ class CTConsistencyModel(nn.Module):
         self.action_dim = action_dim
         self.consistency_lambda = consistency_lambda
         self.sampling_steps = sampling_steps
+        log.info("CTConsistencyModel with time steps %d", sampling_steps)
         self.horizon_steps = horizon_steps
 
         # Set up models
         self.network = network.to(device)
-        # if network_path is not None:
-        #     checkpoint = torch.load(
-        #         network_path, map_location=device, weights_only=True
-        #     )
-        #     self.load_state_dict(checkpoint["model"], strict=False)
-        #     logging.info("Loaded model from %s", network_path)
+        if network_path is not None:
+            checkpoint = torch.load(
+                network_path, map_location=device, weights_only=True
+            )
+            if "ema" in checkpoint:
+                self.load_state_dict(checkpoint["ema"], strict=False)
+                logging.info("Loaded SL-trained policy from %s", network_path)
+            else:
+                self.load_state_dict(checkpoint["model"], strict=False)
+                logging.info("Loaded RL-trained policy from %s", network_path)
         logging.info("initialized CTConsistencyModel")
         logging.info(
             f"Number of network parameters: {sum(p.numel() for p in self.parameters())}"
@@ -60,8 +65,9 @@ class CTConsistencyModel(nn.Module):
         if x is None:
             x = torch.randn((B, self.horizon_steps, self.action_dim), device=device)
 
-        a, b = self.get_c(t)
-        x = a * x + b * self.network(x, t, cond)
+        # a, b = self.get_c(t)
+        # x = a * x + b * self.network(x, t, cond)
+        x = self.network(x, t, cond) # when t=1, x is the output of the network
 
         return Sample(x, None)
 
@@ -72,25 +78,25 @@ class CTConsistencyModel(nn.Module):
         return i / sampling_steps
 
 
-    def consistency_loss(self, ema_model, teacher_model : CTDiffusionModel, sampling_steps, x, cond: dict):
+    def consistency_loss(self, ema_model, teacher_model : CTDiffusionModel, x, cond: dict):
         batch_size = x.shape[0]
-        n = torch.randint(1, sampling_steps, (batch_size, 1, 1)).cuda()
+        n = torch.randint(1, self.sampling_steps, (batch_size, 1, 1)).cuda()
 
-        tn1 = self.get_t_i(n+1, sampling_steps)
+        tn1 = self.get_t_i(n+1, self.sampling_steps)
         x_tn1 = teacher_model.q_sample(x, tn1, None)
-        x_tn_phi = x_tn1 + (self.get_t_i(n, sampling_steps) - self.get_t_i(n+1, sampling_steps)) * teacher_model.pfode_dxt(x_tn1, torch.tensor(self.get_t_i(n+1, sampling_steps)), cond)
+        x_tn_phi = x_tn1 + (self.get_t_i(n, self.sampling_steps) - self.get_t_i(n+1, self.sampling_steps)) * teacher_model.pfode_dxt(x_tn1, torch.tensor(self.get_t_i(n+1, self.sampling_steps)), cond)
 
         # a, b = self.get_c(self.get_t_i(n+1, sampling_steps))
 
         max_std = teacher_model.get_std(torch.tensor(1.0))
-        a = teacher_model.get_mu_coeff(self.get_t_i(n+1, sampling_steps))
-        b = teacher_model.get_std(self.get_t_i(n+1, sampling_steps)) / max_std
-        f_theta = a * x_tn1 + b * self.network(x_tn1, self.get_t_i(n+1, sampling_steps), cond=cond)
+        a = teacher_model.get_mu_coeff(self.get_t_i(n+1, self.sampling_steps))
+        b = teacher_model.get_std(self.get_t_i(n+1, self.sampling_steps)) / max_std
+        f_theta = a * x_tn1 + b * self.network(x_tn1, self.get_t_i(n+1, self.sampling_steps), cond=cond)
 
-        a = teacher_model.get_mu_coeff(self.get_t_i(n, sampling_steps))
-        b = teacher_model.get_std(self.get_t_i(n, sampling_steps)) /max_std
+        a = teacher_model.get_mu_coeff(self.get_t_i(n, self.sampling_steps))
+        b = teacher_model.get_std(self.get_t_i(n, self.sampling_steps)) /max_std
         with torch.no_grad():
-            f_theta_minus = a * x_tn_phi + b * ema_model.network(x_tn_phi, self.get_t_i(n, sampling_steps), cond=cond)
+            f_theta_minus = a * x_tn_phi + b * ema_model.network(x_tn_phi, self.get_t_i(n, self.sampling_steps), cond=cond)
 
         loss = F.mse_loss(f_theta, f_theta_minus, reduction="mean")
         return self.consistency_lambda * loss
